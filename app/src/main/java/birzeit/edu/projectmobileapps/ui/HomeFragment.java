a541c6b7b6cd2aae1362106bbd506e46afb7fbeb
Dana <Danajayousi744@gmail.com>
2022-12-22 21:53:25 +0200

    add bmi calc

--- app/src/main/java/birzeit/edu/projectmobileapps/ui/HomeFragment.java
+++ app/src/main/java/birzeit/edu/projectmobileapps/ui/HomeFragment.java
@@ -1,5 +1,8 @@
 package birzeit.edu.projectmobileapps.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +11,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import birzeit.edu.projectmobileapps.R;
 import birzeit.edu.projectmobileapps.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
+    final double INITIAL_WEIGHT = 75;
+    final double TARGET_WEIGHT = 60;
+    final double CURRENT_WEIGHT = 65;
+    final double HEIGHT = 1.60;
+
+    TextView BMIIndex;
     private FragmentHomeBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,14 +34,33 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-
+        BMIIndex = root.findViewById(R.id.BMI_index);
+        setBMIValue(CURRENT_WEIGHT,HEIGHT);
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    void setBMIValue(double weight, double height) {
+
+        double BMI = weight / (height * height);
+        if (BMI < 18.5) {
+            BMIIndex.setTextColor(getResources().getColor(R.color.blue));
+            BMIIndex.setText(String.format("%.2f",BMI) + "\nUnderweight");
+        } else if (BMI < 25) {
+            BMIIndex.setTextColor(Color.GREEN);
+            BMIIndex.setText(String.format("%.2f",BMI) + "\nNormal");
+        } else if (BMI < 30) {
+            BMIIndex.setTextColor(getResources().getColor(R.color.app_color));
+            BMIIndex.setText(String.format("%.2f",BMI) + "\nOverweight");
+        } else {
+            BMIIndex.setTextColor(Color.RED);
+            BMIIndex.setText(String.format("%.2f",BMI) + "\nObese");
+        }
     }
 }
